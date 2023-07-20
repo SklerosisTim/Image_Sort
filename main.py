980b8c8df967fbf1e9bfafa80f59212603da0d9a
msi <kohai@yandex.ru>
2023-07-20 19:15:19 +0400

    Добавил отображение первой картинки в выбранной папке

--- main.py
+++ main.py
@@ -2,12 +2,13 @@ import pygame
 import sys
 import tkinter
 import tkinter.filedialog
+from os import listdir, path
 
 pygame.init()
 W, H = 800, 600
 display = pygame.display.set_mode((W, H))
 clock = pygame.time.Clock()
-fps = 60
+fps = 30
 path_folder = "<No File Selected>"
 
 
@@ -18,6 +19,21 @@ def prompt_file():
     file_name = tkinter.filedialog.askdirectory(parent=top)
     top.destroy()
     return file_name
+
+
+def image():
+    if path_folder != "<No File Selected>":
+        try:
+            img = listdir(path_folder)[0]
+            path_img = path.join(path_folder, img)
+            original_img = pygame.image.load(path_img).convert()
+            iw, ih = original_img.get_size()
+            scale = min(600 / iw, 600 / ih)
+            new_size = (round(iw * scale), round(ih * scale))
+            resize_img = pygame.transform.smoothscale(original_img, new_size)
+            return resize_img
+        except IndexError:
+            pass
 
 
 def start():
@@ -31,7 +47,8 @@ def start():
                 sys.exit()
             if event.type == pygame.KEYUP and event.key == pygame.K_SPACE:
                 path_folder = prompt_file()
-
+        if path_folder != "<No File Selected>":
+            display.blit(image(), (300 - image().get_rect().centerx, 300 - image().get_rect().centery))
         pygame.display.set_caption(f"{path_folder}")
         pygame.display.update()
         clock.tick(fps)
