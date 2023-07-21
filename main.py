1849da53712b13d0e96e317932cc409e109856d9
msi <kohai@yandex.ru>
2023-07-22 03:23:05 +0400

    Допилил генератор кнопок, сократил код еще на 200 строк!!!

--- main.py
+++ main.py
@@ -72,7 +72,7 @@ def save_load(name_target_folder):
     if not path.isdir(target_folder):
         mkdir(target_folder)
     pygame.image.save(img_original, path.join(target_folder, img_name))
-    print(f'Сохранено в {target_folder}')
+    print(f'{img_name} сохранено в {target_folder}')
     remove(img_path)
     image_load()
 
@@ -87,203 +87,28 @@ def switch_btn_visible(list_btn):
             btn.visible = 1
 
 
-Aespa = Button(W - 250, 10, 240, 45, 'Aespa', manager)
-winter = Button(W - 440, 10, 180, 45, 'Винтер', manager, visible=0)
-giselle = Button(W - 440, 60, 180, 45, 'Жизель', manager, visible=0)
-karina = Button(W - 440, 110, 180, 45, 'Карина', manager, visible=0)
-ningning = Button(W - 440, 160, 180, 45, 'НинНин', manager, visible=0)
-Aespa.group += [winter, giselle, karina, ningning]
-all_girl_btn += [winter, giselle, karina, ningning]
+def button_draw():
+    global all_girl_btn
+    with open('girl_group.txt', 'r', encoding='utf8') as girl_groups:
+        x, y, w, h = 1670, 10, 240, 45
+        for num, line in enumerate(girl_groups):
+            if num == 21:
+                break
+            group = line.split(' ')[0]
+            group_btn = Button(x, y, w, h, group, manager)
+            step = 0
+            up_frame = 0
+            bottom_frame = 22 - len(line.split(' '))
+            if num > bottom_frame:
+                up_frame -= (num - bottom_frame) * 50
+            for girl in line.split(' ')[1:]:
+                member = girl.rstrip()
+                girl_btn = Button(x - 190, y + up_frame + step, w - 60, h, member, manager, visible=0)
+                group_btn.group.append(girl_btn)
+                all_girl_btn.append(girl_btn)
+                step += 50
+            y += 50
 
-Blackpink = Button(W - 250, 60, 240, 45, 'Blackpink', manager)
-jennie = Button(W - 440, 60, 180, 45, 'Дженни', manager, visible=0)
-jisoo = Button(W - 440, 110, 180, 45, 'Джису', manager, visible=0)
-lisa = Button(W - 440, 160, 180, 45, 'Лиса', manager, visible=0)
-rose = Button(W - 440, 210, 180, 45, 'Розэ', manager, visible=0)
-Blackpink.group += [jennie, jisoo, lisa, rose]
-all_girl_btn += [jennie, jisoo, lisa, rose]
-
-Dreamcatcher = Button(W - 250, 110, 240, 45, 'Dreamcatcher', manager)
-gah = Button(W - 440, 110, 180, 45, 'Гахён', manager, visible=0)
-dami = Button(W - 440, 160, 180, 45, 'Дами', manager, visible=0)
-jiu = Button(W - 440, 210, 180, 45, 'Джию', manager, visible=0)
-sua = Button(W - 440, 260, 180, 45, 'Суа', manager, visible=0)
-dongie = Button(W - 440, 310, 180, 45, 'Хандон', manager, visible=0)
-sieon = Button(W - 440, 360, 180, 45, 'Шиён', manager, visible=0)
-yooheon = Button(W - 440, 410, 180, 45, 'Юхён', manager, visible=0)
-Dreamcatcher.group += [gah, dami, jiu, sua, dongie, sieon, yooheon]
-all_girl_btn += [gah, dami, jiu, sua, dongie, sieon, yooheon]
-
-Everglow = Button(W - 250, 160, 240, 45, 'Everglow', manager)
-aisha = Button(W - 440, 160, 180, 45, 'Айша', manager, visible=0)
-iron = Button(W - 440, 210, 180, 45, 'Ирон', manager, visible=0)
-mia = Button(W - 440, 260, 180, 45, 'Миа', manager, visible=0)
-onda = Button(W - 440, 310, 180, 45, 'Онда', manager, visible=0)
-siheon = Button(W - 440, 360, 180, 45, 'Шихён', manager, visible=0)
-eu = Button(W - 440, 410, 180, 45, 'EU', manager, visible=0)
-Everglow.group += [aisha, iron, mia, onda, siheon, eu]
-all_girl_btn += [aisha, iron, mia, onda, siheon, eu]
-
-Fromis_9 = Button(W - 250, 210, 240, 45, 'Fromis_9', manager)
-guri = Button(W - 440, 210, 180, 45, 'Гюри', manager, visible=0)
-jiwon = Button(W - 440, 260, 180, 45, 'Дживон', manager, visible=0)
-jisun = Button(W - 440, 310, 180, 45, 'Джисун', manager, visible=0)
-jiheon = Button(W - 440, 360, 180, 45, 'Джихён', manager, visible=0)
-nageon = Button(W - 440, 410, 180, 45, 'Нагён', manager, visible=0)
-saerom = Button(W - 440, 460, 180, 45, 'Сэром', manager, visible=0)
-haeon = Button(W - 440, 510, 180, 45, 'Хаён', manager, visible=0)
-Fromis_9.group += [guri, jiwon, jisun, jiheon, nageon, saerom, haeon]
-all_girl_btn += [guri, jiwon, jisun, jiheon, nageon, saerom, haeon]
-
-Gidle = Button(W - 250, 260, 240, 45, 'Gidle', manager)
-mieon = Button(W - 440, 260, 180, 45, 'Миён', manager, visible=0)
-minni = Button(W - 440, 310, 180, 45, 'Минни', manager, visible=0)
-soeon = Button(W - 440, 360, 180, 45, 'Соён', manager, visible=0)
-sujin = Button(W - 440, 410, 180, 45, 'Суджин', manager, visible=0)
-shuhua = Button(W - 440, 460, 180, 45, 'Шухуа', manager, visible=0)
-uqi = Button(W - 440, 510, 180, 45, 'Юки', manager, visible=0)
-Gidle.group += [mieon, minni, soeon, sujin, shuhua, uqi]
-all_girl_btn += [mieon, minni, soeon, sujin, shuhua, uqi]
-
-ITZY = Button(W - 250, 310, 240, 45, 'ITZY', manager)
-eji = Button(W - 440, 310, 180, 45, 'Еджи', manager, visible=0)
-lia = Button(W - 440, 360, 180, 45, 'Лиа', manager, visible=0)
-rujin = Button(W - 440, 410, 180, 45, 'Рюджин', manager, visible=0)
-chaeren = Button(W - 440, 460, 180, 45, 'Чeрён', manager, visible=0)
-yuna = Button(W - 440, 510, 180, 45, 'Юна', manager, visible=0)
-ITZY.group += [eji, lia, rujin, chaeren, yuna]
-all_girl_btn += [eji, lia, rujin, chaeren, yuna]
-
-IVE = Button(W - 250, 360, 240, 45, 'IVE', manager)
-woneon = Button(W - 440, 360, 180, 45, 'Вонён', manager, visible=0)
-yujin = Button(W - 440, 410, 180, 45, 'Юджин', manager, visible=0)
-IVE.group += [woneon, yujin]
-all_girl_btn += [woneon, yujin]
-
-Kep1er = Button(W - 250, 410, 240, 45, 'Kep1er', manager)
-dayeon = Button(W - 440, 410, 180, 45, 'Даён', manager, visible=0)
-yenyn = Button(W - 440, 460, 180, 45, 'Енын', manager, visible=0)
-xaotin = Button(W - 440, 510, 180, 45, 'Сяотин', manager, visible=0)
-yujin_kp = Button(W - 440, 560, 180, 45, 'Юджин(k)', manager, visible=0)
-Kep1er.group += [dayeon, yenyn, xaotin, yujin_kp]
-all_girl_btn += [dayeon, yenyn, xaotin, yujin_kp]
-
-LeSserafim = Button(W - 250, 460, 240, 45, 'LeSserafim', manager)
-kazuha = Button(W - 440, 460, 180, 45, 'Казуха', manager, visible=0)
-sakura = Button(W - 440, 510, 180, 45, 'Сакура', manager, visible=0)
-chaewon = Button(W - 440, 560, 180, 45, 'Чевон', manager, visible=0)
-yunjin = Button(W - 440, 610, 180, 45, 'Юнджин', manager, visible=0)
-LeSserafim.group += [kazuha, sakura, chaewon, yunjin]
-all_girl_btn += [kazuha, sakura, chaewon, yunjin]
-
-LOONA = Button(W - 250, 510, 240, 45, 'LOONA', manager)
-vivi = Button(W - 440, 460, 180, 45, 'Виви', manager, visible=0)
-gowon = Button(W - 440, 510, 180, 45, 'Говон', manager, visible=0)
-jinsoul = Button(W - 440, 560, 180, 45, 'Джинсоль', manager, visible=0)
-yojin = Button(W - 440, 610, 180, 45, 'Ёджин', manager, visible=0)
-ives = Button(W - 440, 660, 180, 45, 'Ив', manager, visible=0)
-kimlip = Button(W - 440, 710, 180, 45, 'Ким Лип', manager, visible=0)
-olivia = Button(W - 440, 760, 180, 45, 'Оливия Хе', manager, visible=0)
-hasyl = Button(W - 440, 810, 180, 45, 'Хасыль', manager, visible=0)
-heonjin = Button(W - 440, 860, 180, 45, 'Хёнджин', manager, visible=0)
-heejin = Button(W - 440, 910, 180, 45, 'Хиджин', manager, visible=0)
-choerry = Button(W - 440, 960, 180, 45, 'Чоерри', manager, visible=0)
-chu = Button(W - 440, 1010, 180, 45, 'Чу', manager, visible=0)
-LOONA.group += [vivi, gowon, jinsoul, yojin, ives, kimlip, olivia, hasyl, heonjin, heejin, choerry, chu]
-all_girl_btn += [vivi, gowon, jinsoul, yojin, ives, kimlip, olivia, hasyl, heonjin, heejin, choerry, chu]
-
-Mamamoo = Button(W - 250, 560, 240, 45, 'Mamamoo', manager)
-moon = Button(W - 440, 560, 180, 45, 'Мунбёль', manager, visible=0)
-solar = Button(W - 440, 610, 180, 45, 'Сола', manager, visible=0)
-hwasa = Button(W - 440, 660, 180, 45, 'Хваса', manager, visible=0)
-wheen = Button(W - 440, 710, 180, 45, 'Хвиин', manager, visible=0)
-Mamamoo.group += [moon, solar, hwasa, wheen]
-all_girl_btn += [moon, solar, hwasa, wheen]
-
-Momoland = Button(W - 250, 610, 240, 45, 'Momoland', manager)
-ahin = Button(W - 440, 610, 180, 45, 'Ахин', manager, visible=0)
-jooe = Button(W - 440, 660, 180, 45, 'Джуи', manager, visible=0)
-nancy = Button(W - 440, 710, 180, 45, 'Нэнси', manager, visible=0)
-Momoland.group += [ahin, jooe, nancy]
-all_girl_btn += [ahin, jooe, nancy]
-
-NMIXX = Button(W - 250, 660, 240, 45, 'NMIXX', manager)
-bae = Button(W - 440, 660, 180, 45, 'Бае', manager, visible=0)
-sollun = Button(W - 440, 710, 180, 45, 'Соллюн', manager, visible=0)
-haewon = Button(W - 440, 760, 180, 45, 'Хэвон', manager, visible=0)
-NMIXX.group += [bae, sollun, haewon]
-all_girl_btn += [bae, sollun, haewon]
-
-Oh_My_Girl = Button(W - 250, 710, 240, 45, 'Oh My Girl', manager)
-arin = Button(W - 440, 710, 180, 45, 'Арин', manager, visible=0)
-binnie = Button(W - 440, 760, 180, 45, 'Бинни', manager, visible=0)
-jiho = Button(W - 440, 810, 180, 45, 'Джихо', manager, visible=0)
-mimi = Button(W - 440, 860, 180, 45, 'Мими', manager, visible=0)
-synghee = Button(W - 440, 910, 180, 45, 'Сынхи', manager, visible=0)
-hyojeon = Button(W - 440, 960, 180, 45, 'Хёджон', manager, visible=0)
-yua = Button(W - 440, 1010, 180, 45, 'Юа', manager, visible=0)
-Oh_My_Girl.group += [arin, binnie, jiho, mimi, synghee, hyojeon, yua]
-all_girl_btn += [arin, binnie, jiho, mimi, synghee, hyojeon, yua]
-
-Red_Velvet = Button(W - 250, 760, 240, 45, 'Red Velvet', manager)
-irene = Button(W - 440, 760, 180, 45, 'Айрин', manager, visible=0)
-wendy = Button(W - 440, 810, 180, 45, 'Вэнди', manager, visible=0)
-joy = Button(W - 440, 860, 180, 45, 'Джой', manager, visible=0)
-seulgi = Button(W - 440, 910, 180, 45, 'Сыльги', manager, visible=0)
-Red_Velvet.group += [irene, wendy, joy, seulgi]
-all_girl_btn += [irene, wendy, joy, seulgi]
-
-Rocket_Punch = Button(W - 250, 810, 240, 45, 'Rocket Punch', manager)
-juri = Button(W - 440, 810, 180, 45, 'Джури', manager, visible=0)
-yeonhee = Button(W - 440, 860, 180, 45, 'Ёнхи', manager, visible=0)
-suyun = Button(W - 440, 910, 180, 45, 'Суюн', manager, visible=0)
-yunkyon = Button(W - 440, 960, 180, 45, 'Юнкён', manager, visible=0)
-Rocket_Punch.group += [juri, yeonhee, suyun, yunkyon]
-all_girl_btn += [juri, yeonhee, suyun, yunkyon]
-
-TWICE = Button(W - 250, 860, 240, 45, 'TWICE', manager)
-dahyeon = Button(W - 440, 710, 180, 45, 'Дахён', manager, visible=0)
-jihyo = Button(W - 440, 760, 180, 45, 'Джихё', manager, visible=0)
-mina = Button(W - 440, 810, 180, 45, 'Мина', manager, visible=0)
-momo = Button(W - 440, 860, 180, 45, 'Момо', manager, visible=0)
-nayeon = Button(W - 440, 910, 180, 45, 'Наён', manager, visible=0)
-sana = Button(W - 440, 960, 180, 45, 'Сана', manager, visible=0)
-tzu = Button(W - 440, 1010, 180, 45, 'Тзуи', manager, visible=0)
-TWICE.group += [dahyeon, jihyo, mina, momo, nayeon, sana, tzu]
-all_girl_btn += [dahyeon, jihyo, mina, momo, nayeon, sana, tzu]
-
-VIVIZ = Button(W - 250, 910, 240, 45, 'VIVIZ', manager)
-sinb = Button(W - 440, 860, 180, 45, 'Синби', manager, visible=0)
-umji = Button(W - 440, 910, 180, 45, 'Умджи', manager, visible=0)
-eunha = Button(W - 440, 960, 180, 45, 'Ынха', manager, visible=0)
-sowon = Button(W - 440, 1010, 180, 45, 'Совон', manager, visible=0)
-VIVIZ.group += [sinb, umji, eunha, sowon]
-all_girl_btn += [sinb, umji, eunha, sowon]
-
-WJSN = Button(W - 250, 960, 240, 45, 'WJSN', manager)
-bona = Button(W - 440, 810, 180, 45, 'Бона', manager, visible=0)
-dayeon_wj = Button(W - 440, 860, 180, 45, 'Даён(wj)', manager, visible=0)
-yeorym = Button(W - 440, 910, 180, 45, 'Ерым', manager, visible=0)
-seola = Button(W - 440, 960, 180, 45, 'Сольа', manager, visible=0)
-chenxao = Button(W - 440, 1010, 180, 45, 'Чэнь Сяо', manager, visible=0)
-WJSN.group += [bona, dayeon_wj, yeorym, seola, chenxao]
-all_girl_btn += [bona, dayeon_wj, yeorym, seola, chenxao]
-
-Solo = Button(W - 250, 1010, 240, 45, 'SOLO', manager)
-eyn = Button(W - 440, 460, 180, 45, 'Еын', manager, visible=0)
-yena = Button(W - 440, 510, 180, 45, 'Йена', manager, visible=0)
-chaeyon = Button(W - 440, 560, 180, 45, 'Ли Чеён', manager, visible=0)
-nayn = Button(W - 440, 610, 180, 45, 'Наын', manager, visible=0)
-somi = Button(W - 440, 660, 180, 45, 'Соми', manager, visible=0)
-sunmi = Button(W - 440, 710, 180, 45, 'Сонми', manager, visible=0)
-sohee = Button(W - 440, 760, 180, 45, 'Сохи', manager, visible=0)
-eunbi = Button(W - 440, 810, 180, 45, 'Ынби', manager, visible=0)
-choa = Button(W - 440, 860, 180, 45, 'Чоа', manager, visible=0)
-chungha = Button(W - 440, 910, 180, 45, 'Чонха', manager, visible=0)
-tsuki = Button(W - 440, 960, 180, 45, 'Цуки', manager, visible=0)
-yuri = Button(W - 440, 1010, 180, 45, 'Юри', manager, visible=0)
-Solo.group += [eyn, yena, chaeyon, nayn, somi, sunmi, sohee, eunbi, choa, chungha, tsuki, yuri]
-all_girl_btn += [eyn, yena, chaeyon, nayn, somi, sunmi, sohee, eunbi, choa, chungha, tsuki, yuri]
 
 not_sort = Button(W - 700, 1010, 240, 45, 'Прочее', manager)
 del_img = Button(W - 960, 1010, 240, 45, 'Удалить', manager)
@@ -329,6 +154,7 @@ def start():
 
 
 if __name__ == '__main__':
+    button_draw()
     start()
 
 pygame.quit()
